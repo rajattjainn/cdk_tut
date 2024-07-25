886f4402f1bd2b5a239f2969f8f9df5d35d01da6
Rajat Jain <addresseerajat@gmail.com>
2024-07-25 14:09:54 +0530

    initialise lambda

--- lib/cdk_tut-stack.ts
+++ lib/cdk_tut-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-// import {MyPipelineAppStage} from './stage';
+import { MyPipelineAppStage } from './stage';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -10,7 +10,7 @@ export class CdkTutStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline (this, 'Pipeline', {
+    const pipeline = new CodePipeline (this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep ('Synth', {
         input: CodePipelineSource.gitHub('rajattjainn/cdk_tut', 'main'),
@@ -19,5 +19,15 @@ export class CdkTutStack extends cdk.Stack {
         'npx cdk synth']
       }),
     });     
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage (this, 'test', {
+      env: {account: "058264150249", region: 'ap-southeast-1'}
+    }))
+
+    testingStage.addPost(new ManualApprovalStep ('Manual Approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage (this, 'Prod', {
+      env: {account: "058264150249", region: 'ap-southeast-1'}
+    }))
   }
 }
